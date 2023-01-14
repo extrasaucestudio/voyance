5282fc0e824dc30cd799b354bce4ce181e415777
Jordan Bourbonnais <clonage@gmail.com>
2023-01-14 02:35:30 +0000

    gfdfd

--- files/index.9861557.js
+++ files/index.9861557.js
@@ -3,29 +3,20 @@ if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.que
 return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();
 function s(n){let r=0;const o=i=>{r=i,n.innerHTML=`Voir nos forfaits`};n.addEventListener("click",()=>o(r+1)),o(0)}document.querySelector("#app").innerHTML=`
   <div>
-    
     <img src="https://voyance.cc/files/images/vite.svg" class="logo" alt="Voyance Francophone" />
     <h1>Voyance Francophone!</h1>
     <p class="read-the-docs">
-    <strong>Bienvenue</strong>
-    
     </p>
     <div class="card">
-    
-    <button onclick="openNav()" id="counter" type="button"></button>
-  
+    <strong><button onclick="openNav()" id="counter" type="button"></button></strong>
     </div>
-  
   <!--
+  <strong>Bienvenue</strong>
   <div class="card">
   <a href="https://buy.stripe.com/14k8yn6fvdPVgdW4gi" target="_blank">
   <button id="myNav" type="button"></button>
 </a>
   </div>
-  
   -->
-     
-    
-    
   </div>
 `;s(document.querySelector("#counter"));
